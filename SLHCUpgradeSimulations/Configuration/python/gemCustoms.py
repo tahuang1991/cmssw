47e5a1772b3ee016a4ca3da8b7ff2c584f2bb41c
Jason Lee <jason.lee@cern.ch>
2014-03-24 03:46:10 +0900

    Update gemCustoms.py
    since customise_L1Emulator(process, ptdphi) has two inputs

--- SLHCUpgradeSimulations/Configuration/python/gemCustoms.py
+++ SLHCUpgradeSimulations/Configuration/python/gemCustoms.py
@@ -4,7 +4,7 @@ def customise(process):
     if hasattr(process,'digitisation_step'):
         process=customise_Digi(process)
     if hasattr(process,'L1simulation_step'):
-       process=customise_L1Emulator(process)
+        process=customise_L1Emulator(process,'pt0')
     if hasattr(process,'DigiToRaw'):
         process=customise_DigiToRaw(process)
     if hasattr(process,'RawToDigi'):
