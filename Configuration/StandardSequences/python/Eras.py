46253599185c4b896722840a09e5bc0f3972b985
Vladimir <vladimir.rekovic@cern.ch>
2018-03-24 21:17:54 +0100

    Add Era modifier phase2_trigger.

--- Configuration/StandardSequences/python/Eras.py
+++ Configuration/StandardSequences/python/Eras.py
@@ -42,7 +42,7 @@ class Eras (object):
                            'run2_CSC_2018',
                            'phase2_common', 'phase2_tracker',
                            'phase2_hgcal', 'phase2_muon', 'phase2_timing',
-                           'phase2_timing_layer','phase2_hcal',
+                           'phase2_timing_layer','phase2_hcal', 'phase2_trigger',
                            'trackingLowPU', 'trackingPhase1', 'ctpps_2016', 'trackingPhase2PU140',
                            'tracker_apv_vfp30_2016', 'run2_miniAOD_80XLegacy','run2_miniAOD_94XFall17', 'run2_nanoAOD_92X',
                            'hcalHardcodeConditions', 'hcalSkipPacker']
